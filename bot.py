d357c27f95ec6eaef049d79cbd346e02b0042c10
sadmargo76 <sadmargo76@gmail.com>
2026-03-18 02:47:48 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -812,7 +812,7 @@ def should_send_strength(strength):
     return True
 
 
-def find_breakout_trade(df_1h, df_15m, symbol: str):
+def find_breakout_trade(df_15m, df_1h, trend, symbol):
     if len(df_1h) < BREAKOUT_LOOKBACK + 5 or len(df_15m) < 30:
         return None
 
